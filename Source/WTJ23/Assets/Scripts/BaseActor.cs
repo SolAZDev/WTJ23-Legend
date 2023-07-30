f317412c798ae95af54e1d90705b92949842fb4a
JblazeTK <119426962+JblazeTK@users.noreply.github.com>
2023-07-30 02:35:11 -0400

    StopMoving on BaseActor

--- Source/WTJ23/Assets/Scripts/BaseActor.cs
+++ Source/WTJ23/Assets/Scripts/BaseActor.cs
@@ -11,7 +11,7 @@ public class BaseActor : MonoBehaviour
     [Range(10,50)] public int Health=10;
     [Range(-1,1)]  public float Nervouseness=0;
     public float Speed=3f, RunSpeed=5f;
-
+    public bool CanMove;
     [HideInInspector]public bool hasSeenEnemy=false;
     public NavMeshAgent navMeshAgent;
     public Animator animator;
@@ -28,8 +28,17 @@ public class BaseActor : MonoBehaviour
         if (other.name.Contains("Trap"))
         {
             Health -= 10;
+            StopMoving();
         }
     }
 
+    public void SendAbility(string args)
+    {
+        SendMessage("Ability" + args);
+    }
 
+    IEnumerator StopMoving()
+    {
+        yield return new WaitForSeconds(Random.Range(3,5));
+    }
 }
